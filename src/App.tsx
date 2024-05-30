b761ccb6f505ea92ebcda80ffa1afa87cb40f7dc
DioChuks <diochuks65@gmail.com>
2024-05-30 20:30:21 +0100

    fix: breakpoints modified

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import MenuBtn from "./components/MenuBtn";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-between w-full gap-10 px-4 py-4 mb-8 md:h-screen top-section">
+      <div className="flex flex-col items-center justify-between gap-20 px-4 py-4 mb-8 md:mx-8 top-section">
         <header className="relative flex items-center justify-between w-full gap-10">
           <div className="flex items-center gap-3">
             <img src={Logo} alt="Logo" />
@@ -34,7 +34,7 @@ function App() {
           <MenuBtn />
         </header>
         <div className="flex flex-col items-center text-center md:w-5/6 hero-section">
-          <h1 className="text-3xl font-bold md:text-6xl">
+          <h1 className="text-3xl font-bold lg:text-6xl">
             Unlock <span className="text-forest-green-500">10x</span> Growth in
             Campus Communities
           </h1>
@@ -62,8 +62,8 @@ function App() {
             <img src={Muzingo} alt="Muzingo" className="w-2/3 h-16 md:w-auto" />
           </div>
           <div className="w-[fit-content] h-16 bg-[#0D0F36] flex items-center justify-center rounded-[20px]">
-            <img src={Sambe1} alt="Sambe1" className="flex-1" />
-            <img src={Sambe2} alt="Sambe2" className="flex-1"/>
+            <img src={Sambe1} alt="Sambe1" className="w-1/3" />
+            <img src={Sambe2} alt="Sambe2" className="w-1/2"/>
           </div>
           {/* next founder logos */}
           <div className="flex items-center justify-center">
@@ -79,7 +79,7 @@ function App() {
       </div>
       <div className="flex justify-center w-full py-4 bg-cr-gray-200 second-section">
         <div className="flex flex-col items-center w-5/6 gap-5 mt-10">
-          <h3 className="text-3xl font-bold text-center md:text-5xl">
+          <h3 className="text-3xl font-bold text-center lg:text-5xl">
             Accelerate Your Growth Across Campus Communities
           </h3>
           <p className="mb-3 text-center text-cr-gray-600">
@@ -89,37 +89,37 @@ function App() {
             through our network of student marketers. Whether virtual or
             physical, we ensure your campaign's success on any campus, anywhere
           </p>
-          <div className="grid w-full grid-rows-2 gap-10 md:grid-cols-2">
+          <div className="grid w-full grid-rows-2 gap-10 lg:grid-cols-2">
             <AccelCard
               title="End-to-End Campaign Management"
               description="We handle every aspect of your campaign, from strategy to student-creator management, allowing you to focus on your business while we accelerate your growth efficiently"
-              cnames="md:h-[400px] bg-cr-pink-300"
+              cnames="lg:h-[400px] bg-cr-pink-300"
               textWidth="w-[70%]"
               image={GroupImg1}
             />
             <AccelCard
               title="First-Class Customer Support"
               description="Our marketers double as your first line of customer support, aiding new users in onboarding onto your platform or accessing your services. This ensures a seamless first-user experience, enhancing user satisfaction."
-              cnames="md:h-[400px] bg-cr-light-green"
+              cnames="lg:h-[400px] bg-cr-light-green"
               textWidth="w-[70%]"
               image={GroupImg2}
             />
             <AccelCard
               title="Insightful User Feedback"
               description="Our campaigns gather invaluable product, and market insights from your target customers through our experienced student marketers. These insights are compiled into a comprehensive document, empowering you to enhance your product value."
-              cnames="md:h-[250px] bg-cr-gray-300 px-2"
+              cnames="lg:h-[250px] bg-cr-gray-300 px-2"
               textWidth="text-left grid w-5/6 place-items-start"
               image=""
             />
             <AccelCard
               title="Access to Underserved Markets"
               description="Utilizing our network of student marketers, we can penetrate under-served markets with campaigns that are not only 10x more affordable but also highly effective, providing you with access to untapped opportunities."
-              cnames="md:h-[250px] bg-cr-yellow-200 px-2"
+              cnames="lg:h-[250px] bg-cr-yellow-200 px-2"
               textWidth="text-left grid w-5/6 place-items-start"
               image=""
             />
-            <div className="col-span-2 md:h-[350px] bg-white h-52 rounded-xl flex justify-center items-center md:-mt-40">
-              <div className="grid grid-cols-2 gap-4 place-items-center h-2/3">
+            <div className="col-span-2 lg:h-[350px] bg-white h-[300px] rounded-xl flex justify-center items-center md:-mt-40">
+              <div className="grid grid-rows-2 gap-4 lg:grid-cols-2 place-items-center h-2/3">
                 <div className="w-[85%] h-full justify-center flex flex-col">
                   <h6 className="mb-4 text-2xl font-bold">
                     Comprehensive Marketing Analytics
@@ -147,7 +147,7 @@ function App() {
                       <option value="7">Last 7 days</option>
                     </select>
                   </div>
-                  <div className="flex items-center justify-between md:w-[80%] w-full gap-4">
+                  <div className="flex items-center justify-between lg:w-[80%] w-full gap-4">
                     <img src={PieChart} alt="chart" />
                     <ul className="flex flex-col justify-start">
                       <li className="text-[#FF3A29] list-disc text-xl">
