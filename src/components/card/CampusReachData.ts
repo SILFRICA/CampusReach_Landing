e0cfc3c47ce11e422866a88cd6fb9d5a2306d0e9
DioChuks <diochuks65@gmail.com>
2024-06-01 00:33:24 +0100

    feat: campus reach table comparison

---
+++ src/components/card/CampusReachData.ts
@@ -0,0 +1,37 @@
+export const campusReachTableData = [
+    {
+        pros: "Cost",
+        cr: "Minimum budget: #50,000 + 10% campaign fee",
+        traditional: "Minimum budget: #500,000 + fixed fee"
+    },
+    {
+        pros: "Time to results",
+        cr: "Within 2 weeks",
+        traditional: "Long planning and strategy"
+    },
+    {
+        pros: "ROI",
+        cr: "High",
+        traditional: "Medium"
+    },
+    {
+        pros: "Product Insight/User Feedback",
+        cr: "Insight compiled from each campaign",
+        traditional: "Mostly provide user data without insight"
+    },
+    {
+        pros: "Access to Local Communities",
+        cr: "Engage local communities through targeted outreach",
+        traditional: "No access"
+    },
+    {
+        pros: "Physical Presence",
+        cr: "Can conduct campaigns online or physically",
+        traditional: "Limited availability for physical campaigns"
+    },
+    {
+        pros: "Scalability",
+        cr: "Scale campaigns across multiple campuses",
+        traditional: "Limited scalability or reach"
+    },
+];
--- src/components/card/CampusReachVs.tsx
+++ src/components/card/CampusReachVs.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/campusreachlogo.svg";
+import { campusReachTableData } from "./CampusReachData";
+
+interface TableData {
+  pros: string;
+  cr: string;
+  traditional: string;
+}
 
 const CampusReachVs: React.FC = () => {
   return (
     <div className="w-[85%] mx-auto">
-        <small className='text-center'>CampusReach Vs Others</small>
+      <div className="w-full text-center">
+        <small className="text-gray-300">CampusReach Vs Others</small>
+        <h3 className="text-2xl font-bold lg:text-5xl">
+          Purpose built for Startups
+        </h3>
+      </div>
+      <br />
+      <section className="container p-6 mx-auto mt-6 font-mono lg:mt-8">
+        <div className="w-full mb-8 overflow-hidden rounded-lg">
+          <div className="w-full overflow-x-auto">
+            <table className="w-full">
+              <thead>
+                <tr className="font-semibold tracking-wide text-center text-gray-900 border-b border-gray-600 text-md">
+                  <th className="px-4 py-3">Benefits</th>
+                  <th className="flex items-center justify-center gap-3 px-4 py-3 border bg-[#F1F9F5]">
+                    <img src={Logo} alt="Logo" />
+                    <h1 className="text-sm font-bold text-forest-green-900">
+                      CampusReach
+                    </h1>
+                  </th>
+                  <th className="px-4 py-3">Traditional Agencies</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {campusReachTableData.map((list: TableData, index: number) => (
+                  <tr key={index} className="text-center text-gray-700">
+                    <td className="h-24 px-4 py-3 text-base font-medium border">
+                      {list.pros}
+                    </td>
+                    <td className="h-24 px-4 py-3 font-normal border text-base bg-[#F1F9F5]">
+                      {list.cr}
+                    </td>
+                    <td className="h-24 px-4 py-3 text-base font-normal border">
+                      {list.traditional}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default CampusReachVs
+export default CampusReachVs;
